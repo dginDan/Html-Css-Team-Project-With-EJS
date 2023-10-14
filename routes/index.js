ee6f20dbf04105acc55dacdac260e870327d67ea
Jessica <jessdub27@outlook.com>
2023-10-13 21:24:19 -0400

    Ajout d'une section panier dans la page shop, ajout de boutons de navigation et ajout de l'inventaire du joueur

--- routes/index.js
+++ routes/index.js
@@ -6,8 +6,7 @@
 const express = require('express');
 const passport = require('passport');
 const Mots=require('../models/Mots');
-
-
+const Usagers = require('../models/Usagers');
 
 const fonctRoute = require('../config/fonctRoute').fonctRoute;
 
@@ -36,14 +35,21 @@ router.post('/userLogin', (requete, reponse, next) => {
     }
 );
 //page menu
-router.get('/village', isAuthentified, (requete, reponse)=>{
-    const user = requete.user;
-    console.log(requete.user);
-    reponse.render(`village`, {
-        'title': 'village',
-        user : user 
+router.get('/village', isAuthentified, (requete, reponse) => {
+    Usagers.findOne({ email: requete.session.passport.user })
+    .populate('inventaire')
+    .then(user => {
+        reponse.render('village', {
+            'title': 'village',
+            'user': user 
+        });
+    })
+    .catch(err => {
+        console.error(err);
+        reponse.status(500).send('Erreur lors de la récupération des données.');
     });
 });
+
 router.get('/taverne', isAuthentified, (requete, reponse)=>{
     const user = requete.user;
     reponse.render(`taverne`, {
--- routes/items.js
+++ routes/items.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const {isAuthentified} = require('../config/auth');
 const Items = require('../models/Items');
+const Usagers = require('../models/Usagers');
+const mongoose = require('mongoose');
 
 const router = express.Router();
-
 
 // Affichage de la page Shop
 router.get('/shop', isAuthentified, (requete, reponse) => {
@@ -17,15 +18,188 @@ router.get('/shop', isAuthentified, (requete, reponse) => {
 
     Items.find(query).exec()
         .then(items => {
+            // Si le panier est vide, affiche simplement la boutique
+            if (!requete.session.cart || requete.session.cart.items.length === 0) {
+                return Promise.resolve({ items: items, cart: null });
+            }
+
+            // Sinon récupérez les détails de chaque item dans le panier
+            const itemDetailsPromises = requete.session.cart.items.map(cartItem => {
+                return Items.findById(cartItem.itemId).then(item => {
+                    cartItem.nom = item.nom;
+                    cartItem.prix = item.prix;
+                    return cartItem;
+                });
+            });
+
+            return Promise.all(itemDetailsPromises).then(cartItems => {
+                return { items: items, cart: { items: cartItems, total: requete.session.cart.total } };
+            });
+        })
+        .then(data => {
             reponse.render('shop', {
-                items: items,
-                user:user
+                items: data.items,
+                user: user,
+                cart: data.cart
             });
         })
         .catch(err => {
             console.error(err);
             reponse.status(500).send('Erreur lors de la récupération des articles.');
         });
+});
+
+router.post('/vider-panier', isAuthentified,  (requete, reponse) => {
+    // Réinitialise le panier dans la session
+    requete.session.cart = {
+        items: [],
+        total: 0
+    };
+
+    // Sauvegarde les modifications dans la session
+    requete.session.save(err => {
+        if (err) {
+            console.error("Erreur lors de la sauvegarde de la session:", err);
+            return reponse.status(500).send('Erreur lors de la sauvegarde du panier.');
+        }
+        reponse.redirect('shop');
+    });
+});
+
+router.post('/acheter-panier', isAuthentified, (requete, reponse) => {
+    // Obtenir le panier de la session
+    const cart = requete.session.cart;
+    if (!cart || cart.items.length === 0) {
+        return reponse.redirect('/shop');
+    }
+
+    Usagers.findById(requete.user._id)
+    .then(user => {
+        if (!user) {
+            throw new Error('Utilisateur non trouvé');
+        }
+
+        // Vérifie si le joueur a suffisamment de gold pour tous les items du panier
+        if (user.gold < cart.total) {
+            throw new Error('Solde en gold insuffisant');
+        }
+
+        // Déduis le coût total des items du solde du joueur
+        user.gold -= cart.total;
+
+        // Pour chaque items du panier
+        cart.items.forEach(cartItem => {
+            // Vérifie si l'item est déjà dans l'inventaire du joueur
+            const indexItem = user.inventaire.findIndex(i => i.item && i.item.toString() === cartItem.itemId);
+
+            if (indexItem !== -1) {
+                // L'item est déjà dans l'inventaire du joueur, augmente la quantité
+                user.inventaire[indexItem].quantite += cartItem.quantite;
+            } else {
+                // Ajoute le nouvel item à l'inventaire du joueur
+                user.inventaire.push({ item: cartItem.itemId, quantite: cartItem.quantite });
+            }
+        });
+
+        return user.save();
+    })
+    .then(() => {
+        // Réinitialise le panier dans la session après l'achat réussi
+        requete.session.cart = { items: [], total: 0 };
+        reponse.redirect('/inventaire');
+    })
+    .catch(err => {
+        console.error(err);
+        reponse.status(500).send(err.message);
+    });
+});
+
+router.post('/ajouter-au-panier/:id', isAuthentified, (requete, reponse) => {
+    const itemId = requete.params.id;
+
+    // initialise le panier s'il n'existe pas dans la session
+    if(!requete.session.cart) {
+        requete.session.cart = {
+            items: [],
+            total: 0
+        };
+    }
+
+    // Trouve l'item en fonction de l'ID
+    Items.findById(itemId)
+        .then(item => {
+            if(!item) {
+                console.error("Article non trouvé pour l'ID:", itemId);
+                throw new Error('Article non trouvé');
+            }
+
+            // Vérifie si l'item est déjà dans le panier
+            const existingItem = requete.session.cart.items.find(i => i.itemId.toString() === itemId);
+
+            if(existingItem) {
+                // Augmente simplement la quantité si l'item est déjà dans le panier
+                existingItem.quantite += 1;
+            } else {
+                // Sinon, ajoute un nouvel élément au panier
+                requete.session.cart.items.push({
+                    itemId: itemId,
+                    quantite: 1,
+                    nom: item.nom, 
+                    prix: item.prix
+                });
+            }
+
+            // Mets à jour le total du panier
+            requete.session.cart.total += item.prix;
+
+            // Sauvegarde le panier dans la session
+            requete.session.save(err => {
+                if(err) {
+                    console.error("Erreur lors de la sauvegarde de la session:", err);
+                    return reponse.status(500).send('Erreur lors de la sauvegarde du panier.');
+                }
+                reponse.redirect('/items/shop');
+            });
+        })
+        .catch(err => {
+            console.error(err);
+            reponse.status(500).send(err.message);
+        });
+});
+
+router.post('/supprimer/:id', isAuthentified, (requete, reponse) => {
+    const itemId = requete.params.id;
+
+    Usagers.findOne({ email: requete.session.passport.user })
+    .then(user => {
+        if (!user) {
+            throw new Error('Utilisateur non trouvé');
+        }
+
+        // Trouve l'item dans l'inventaire du joueur
+        const itemEntry = user.inventaire.find(entry => entry.item.toString() === itemId);
+        
+        if (itemEntry) {
+            // Si la quantité est à 1, retire directement l'item de l'inventaire
+            if (itemEntry.quantite === 1) {
+                const index = user.inventaire.indexOf(itemEntry);
+                user.inventaire.splice(index, 1);
+            } else {
+                // Sinon, réduis simplement la quantité de 1
+                itemEntry.quantite -= 1;
+            }
+
+            return user.save();
+        }
+        throw new Error('Item non trouvé dans l\'inventaire');
+    })
+    .then(() => {
+        reponse.redirect('/inventaire');
+    })
+    .catch(err => {
+        console.error(err);
+        reponse.status(500).send(err.message);
+    });
 });
 
 module.exports = router;
